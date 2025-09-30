b4433390ad90f188cbd57a40bcc738fdc1592a5d
username <wecp-dummy@gmail.com>
2025-09-30 05:48:00 +0000

    Entity changes made in 4 files

--- server/src/main/java/com/wecp/insurance_claims_processing_system/entity/Adjuster.java
+++ server/src/main/java/com/wecp/insurance_claims_processing_system/entity/Adjuster.java
@@ -1,11 +1,13 @@
 package com.wecp.insurance_claims_processing_system.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Table(name = "adjuster") 
 @Entity
 public class Adjuster extends User {
-
+@OneToMany(mappedBy = "adjuster")
+    private Claim claim;
 }
