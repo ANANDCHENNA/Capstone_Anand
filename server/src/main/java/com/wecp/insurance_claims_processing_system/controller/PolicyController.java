d447c73c1793e7b076a0b3c636547139e2879f67
username <wecp-dummy@gmail.com>
2025-10-07 13:08:38 +0000

    Policy Created nand View Policy Working

--- server/src/main/java/com/wecp/insurance_claims_processing_system/controller/PolicyController.java
+++ server/src/main/java/com/wecp/insurance_claims_processing_system/controller/PolicyController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/policy")
+@RequestMapping("/api/policies")
 public class PolicyController {
 
     @Autowired
